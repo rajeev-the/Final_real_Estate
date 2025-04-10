e6a6c52f018b6ecfcf86e413a247b1352b6a2c9e
Rajeev <rajeevranjan9560807144@gmail.com>
2025-04-10 16:36:33 +0530

    update newwww  Frontend

--- src/Pages/ViewLand.jsx
+++ src/Pages/ViewLand.jsx
@@ -284,7 +284,7 @@ const ViewLand = () => {
   {/* Responsive Map */}
   <div className="w-full max-w-4xl h-[450px]">
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d42157498.47345266!2d60.94156072887267!3d19.69240573692257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff5d23cbb0f%3A0xe1b092d74e10e9c5!2sIndia!5e0!3m2!1sen!2sin!4v1744037809273!5m2!1sen!2sin"
+      src={data.map_link}
       width="100%"
       height="100%"
       style={{ border: 0 }}
