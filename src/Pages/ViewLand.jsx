956a65200acaf839e82c5e09ee8181be2ea7e0fa
Rajeev <rajeevranjan9560807144@gmail.com>
2025-04-11 11:00:09 +0530

    update newwww  Frontend

--- src/Pages/ViewLand.jsx
+++ src/Pages/ViewLand.jsx
@@ -8,7 +8,7 @@ import axios from "axios";
 import {  toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const ViewLand = () => {
+const ViewLand = ({openLogin}) => {
   const { id } = useParams();
   const { property } = useAppContext();
   const url = "https://finalbackend111.pythonanywhere.com/api/";
@@ -60,7 +60,7 @@ const ViewLand = () => {
     if (!isUser) {
       
       showErrorToast("Please login to continue.");
-      navigate("/login");
+      openLogin()
       return;
     }
 
